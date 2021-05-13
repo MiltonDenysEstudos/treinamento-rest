47ba222af66a8d24258d51205590f8f06512c656
mDenys7 <milton.silva17@fatec.sp.gov.br>
2021-05-13 00:14:18 -0300

    deveIncluirContaComSucesso

--- src/main/java/br/mdenys/rest/tests/BarrigaTest.java
+++ src/main/java/br/mdenys/rest/tests/BarrigaTest.java
@@ -1,10 +1,13 @@
 package br.mdenys.rest.tests;
 
+import static io.restassured.RestAssured.given;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
 import br.mdenys.rest.core.BaseTest;
-
-import static io.restassured.RestAssured.given;
 
 public class BarrigaTest extends BaseTest{
 
@@ -18,5 +21,31 @@ public class BarrigaTest extends BaseTest{
 			.statusCode(401)
 		;
 		
+	}
+	@Test
+	public void deveIncluirContaComSucesso() {		
+		Map<String, String> login = new HashMap<String, String>();
+		login.put("email", "skateboards35@gmail.com");
+		login.put("senha", "12345");
+
+		String token = given()
+			.body(login)
+		.when()
+			.post("/signin")
+		.then()
+			.statusCode(200)
+			.extract().path("token")
+			;
+		System.out.println(token);
+
+		given()
+			.header("Authorization","JWT " + token)//ao inves de JWT pode ser bearer//nao esquecer espaco depois JWT
+			.body("{\"nome\": \"conta qualquer\"}")
+		.when()
+			.post("/contas")
+		.then()
+			.statusCode(201)
+		;
+
 	}
 }
